eceda000ad6e02ef8dc73ce1977030dfcc7d98d8
Yanin.A.V <aleksei.yanin.qa@gmail.com>
2024-10-08 16:54:54 +0300

    Финальная ДЗ

--- src/main/java/com/edu/DefaultCustomArrayList.java
+++ src/main/java/com/edu/DefaultCustomArrayList.java
@@ -1,51 +1,107 @@
 package com.edu;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class DefaultCustomArrayList<E> implements CustomArrayList<E> {
+    private Object[] elements;
+    private int size;
+    private static final int INITIAL_CAPACITY = 5;
 
     public DefaultCustomArrayList() {
+        elements = new Object[INITIAL_CAPACITY];
+        size = 0;
     }
 
     @Override
     public boolean add(E element) {
+        if (size == elements.length) {
+            resize();
+        }
+        elements[size++] = element;
         return true;
     }
 
     @Override
     public boolean remove(E element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                removeAt(i);
+                return true;
+            }
+        }
         return false;
     }
 
-    private void remove(int index) {
+
+    private void removeAt(int index) {
+        System.arraycopy(elements, index + 1, elements, index, size - index - 1);
+        elements[--size] = null;
     }
 
     @Override
     public E get(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return (E) elements[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void clear() {
+        Arrays.fill(elements, 0, size, null);
+        size = 0;
     }
 
     @Override
     public boolean contains(E element) {
-        return false;
+        return indexOf(element) >= 0;
+    }
+
+
+    private int indexOf(E element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(element)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() {
+                return currentIndex < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return (E) elements[currentIndex++];
+            }
+        };
+    }
+
+
+    private void resize() {
+        int newCapacity = elements.length * 2;
+        elements = Arrays.copyOf(elements, newCapacity);
     }
 }
